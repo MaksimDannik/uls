86794a89cc56c1e3bc79b918328a53d5721ba412
Maksym Dannyk <mdannyk@e2r4p8.unit.ua>
2020-02-12 22:46:39 +0200

    need make sort. function in mx_sort_asci

--- src/mx_parse.c
+++ src/mx_parse.c
@@ -1,60 +1,50 @@
 #include "uls.h"
 
-static int countread(t_lit **arg, t_head *head) {
-    int count = 0;
-    t_lit *args = *arg;
-    DIR *dir;
-    struct dirent *ds;
+static sizearr(t_lit **arg) {
+    int i = 0;
+    
+    while (arg[i])
+        i++;
+    return i;
+}
 
-    if (MX_IS_DIR(args->t_st.st_mode)) {
-        if ((dir = opendir(args->name)) != NULL) {
-            while ((ds = readdir(dir)) != NULL)
-                count++;
-            closedir(dir);
-        }
-        else {
-            (*arg)->error = mx_strdup(strerror(errno));
-            return -1;
+static void mx_sort_asci(t_lit ***arg) {
+    t_lit **args = *arg;
+    int size = sizearr(args);
+
+    for (int i = 0; i < size; i++) {
+        for (int j = i + 1; j < size; j++) {
+            
         }
     }
-    return count;
 }
 
-void mx_opendir(t_lit ***args, t_head *head) {
-    //DIR *dir;
-   // struct dirent *dd;
-    int count = 0;
-
-if (head) {
-
-
-}
-    for (int i = 0; (*args)[i] != NULL; i++) {
-        count = countread(&(*args)[i], head);
+static void print_err(t_lit **arg) {
+    for (int i = 0; arg[i] != NULL; i++) {
+        if (arg[i]->error) {
+            mx_printerr("ls: ");
+            mx_printerr(arg[i]->name);
+            mx_printerr(": ");
+            mx_printerr(arg[i]->error);
+            mx_printerr("\n");
+        }
     }
-
 }
-
 
 void mx_parse(int argc, char **argv, t_head *head) {
     int count = head->count_flags;
     t_lit **args = mx_getname(argc, argv, count);
 
+    mx_sort_asci(&args);//  допилить
+    print_err(args);
+ 
+    
 
-if (head) {
-    
-}
-    if (args)
-        mx_opendir(&args, head);
-    
     // for (int i = 0; args[i] != NULL; i++) { // check **args // i need make sort after make this functions.
     //     // mx_printerr("ls: ");
     //     mx_printerr(args[i]->name);
-    // mx_printerr("s: ");
-    // mx_printerr(args[i]->error);
     // }
-
-
     return;
 }
+
 
